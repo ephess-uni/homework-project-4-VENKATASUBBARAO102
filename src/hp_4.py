43f504a6fa49af609796f3579e62ddb8192ff71b
VENKATASUBBARAO102 <149022422+VENKATASUBBARAO102@users.noreply.github.com>
2023-12-08 18:25:25 -0600

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,27 +8,48 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    return [datetime.strptime(i,"%Y-%m-%d").strftime("%d %b %Y") for i in old_dates]
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    start_date = datetime.strptime(start,"%Y-%m-%d")
+    return [start_date + timedelta(days=i) for i in range(n)]
+   
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    start_date = datetime.strptime(start_date,"%Y-%m-%d")
+    return [(start_date+timedelta(days=pos),val) for pos,val in enumerate(values)]
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    late_fees_dict=defaultdict(float)
+    with open(infile,'r') as file:
+        reader = DictReader(file)
+        for row in reader:
+            date_due = datetime.strptime(row['date_due'],"%m/%d/%Y")
+            date_returned = datetime.strptime(row['date_returned'],"%m/%d/%Y")
+            if date_returned > date_due:
+                days_late = (date_returned - date_due).days
+                late_fee= round(days_late*0.25,2)
+                late_fees_dict[row['patron_id']] += late_fee
+            else:
+                late_fees_dict[row['patron_id']]= 0.00
+    with open(outfile,'w',newline='') as file:
+        cols = ['patron_id','late_fees']
+        late_fees_list=[{'patron_id':key,'late_fees':str(value)+'0' if len(str(value).split('.')[-1]) !=2 else str(value)} for key,value in late_fees_dict.items()]
+        writer = DictWriter(file,cols)
+        writer.writeheader()
+        writer.writerows(late_fees_list)
+       
 
 
 # The following main selection block will only run when you choose
@@ -39,7 +60,7 @@ def fees_report(infile, outfile):
 # under the data directory.
 
 if __name__ == '__main__':
-    
+   
     try:
         from src.util import get_data_file_path
     except ImportError:
@@ -55,3 +76,4 @@ if __name__ == '__main__':
     # Print the data written to the outfile
     with open(OUTFILE) as f:
         print(f.read())
+
